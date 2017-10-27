787a09acfb45454d5acdaed874f48b748797930c
joevansanttos <joevansanttos@gmail.com>
2017-10-27 07:23:34 -0300

    class database

---
+++ production/class/Conexao.php
@@ -0,0 +1,15 @@
+<?php 
+
+  class Conexao {
+    
+    function __construct(){
+     $this->conecta();
+   }
+   
+    function conecta() {
+      $conexao = mysqli_connect("127.0.0.1", "root", "", "manager");
+      mysqli_set_charset($conexao, "utf8");
+      return $conexao;
+    }
+  }
+?>
--- production/factory/UsuarioFactory.php
+++ production/factory/UsuarioFactory.php
@@ -1,12 +1,12 @@
 <?php
 	require "../class/Usuario.php";
 	require "../dao/ProfissaoDao.php";
+	require_once "../class/Conexao.php";
 
 	class UsuarioFactory {
 
-		public function criaUsuario($params) {	
-			$conexao = mysqli_connect("localhost", "root", "", "manager");
-			mysqli_set_charset($conexao, "utf8");		
+		public function criaUsuario($params) {
+			$conexao = new Conexao();	
 			$nome = $params['nome'];
 			$email = $params['email'];		
 			$senha = $params['senha'];
@@ -16,7 +16,7 @@
 			$cidade = $params['cidade'];
 			$telefone = $params['telefone'];
 			$id_profissao = $params['profissao'];
-			$profissaoDao = new ProfissaoDao($conexao);
+			$profissaoDao = new ProfissaoDao($conexao->conecta());
 			$profissao = $profissaoDao->buscaProfissao($id_profissao);			
 			return new Usuario($nome ,$email, $senha, $sobrenome, $sexo, $estado, $cidade, $telefone, $profissao);
 		}	
